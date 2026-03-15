1776fb92cfdc50bbdeba40ae47db619278d6937c
areysid <sidharthmalpani@gmail.com>
2026-03-15 22:43:18 +0530

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Sidharth Malpani — Portfolio",
+  title: "Sidharth Malpani - Portfolio",
   description: "DevOps Enthusiast & Full-Stack Explorer",
 };
 
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
